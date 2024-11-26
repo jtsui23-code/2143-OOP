305fe443b7c12d364a79ae47e2ae7bdfca933315
Jack <Jacktsui23@gmail.com>
2024-11-25 21:21:33 -0600

    Finished the activity for lesson_01

--- Assignments/10-P03/JackTsuiLesson_01.cpp
+++ Assignments/10-P03/JackTsuiLesson_01.cpp
@@ -5,8 +5,13 @@ int main() {
 // Create a window with a title and dimensions (800x600)
 sf::RenderWindow window(sf::VideoMode(1024, 768), "Jack Tsui's KnuckleBones");
 
+sf::Clock clock;
+
     // Main game loop
     while (window.isOpen()) {
+        sf:: Time elasped = clock.restart();
+        float fps = 1.0f/elasped.asSeconds();
+        std:: cout << "FPS: " << fps << std:: endl;
         // Event handling
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -18,7 +23,7 @@ sf::RenderWindow window(sf::VideoMode(1024, 768), "Jack Tsui's KnuckleBones");
         }
 
         // Clear the window with a specific color
-        window.clear(sf::Color::Black);
+        window.clear(sf::Color::Red);
 
         // Display the contents of the window
         window.display();
