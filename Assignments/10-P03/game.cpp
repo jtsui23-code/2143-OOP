b49f64af2135202e34db18004d165dc3b72b089b
Jack Tsui <JackTsui23@gmail.com>
2024-12-09 23:43:58 -0600

    Fixed formating in code

--- Assignments/10-P03/game.cpp
+++ Assignments/10-P03/game.cpp
@@ -260,38 +260,38 @@ class Game
     }
 
     void inputNames(sf::Event event)
-{
-    if (!nameEntered[i] && i < 2) 
     {
-        if (event.type == sf::Event::TextEntered) {
-            if (event.text.unicode == '\b') {
-                // Handle backspace
-                if (!userInput[i].empty()) {
-                    userInput[i].pop_back();
-                }
-            } else if (event.text.unicode == '\r') {
-                // Handle enter
-                nameEntered[i] = true;
+        if (!nameEntered[i] && i < 2) 
+        {
+            if (event.type == sf::Event::TextEntered) {
+                if (event.text.unicode == '\b') {
+                    // Handle backspace
+                    if (!userInput[i].empty()) {
+                        userInput[i].pop_back();
+                    }
+                } else if (event.text.unicode == '\r') {
+                    // Handle enter
+                    nameEntered[i] = true;
 
-                if (i == 0) {
-                    displayName.setString(userInput[0]);
-                }
-                if (i == 1) {
-                    displayName2.setString(userInput[1]);
+                    if (i == 0) {
+                        displayName.setString(userInput[0]);
+                    }
+                    if (i == 1) {
+                        displayName2.setString(userInput[1]);
+                    }
+
+                    if (i < 1) {
+                        i++;
+                    }
+                } else if (event.text.unicode < 128) {
+                    // Handle valid characters
+                    userInput[i] += static_cast<char>(event.text.unicode);
                 }
 
-                if (i < 1) {
-                    i++;
-                }
-            } else if (event.text.unicode < 128) {
-                // Handle valid characters
-                userInput[i] += static_cast<char>(event.text.unicode);
+                nameText.setString(userInput[i]);
             }
-
-            nameText.setString(userInput[i]);
         }
     }
-}
 
 
     
