785f057ba19cecfab8386e28dd4d47fa2dc921cf
Jack Tsui <JackTsui23@gmail.com>
2024-12-10 18:53:06 -0600

    Changed heading

--- Assignments/10-P03/game.cpp
+++ Assignments/10-P03/game.cpp
@@ -23,7 +23,7 @@
 *      ./game [filename]
 * 
 *  Files:            
-*                     game.cpp
+*                    game.cpp
 *                    game.exe 
 *                    grid.hpp
 *                    diceRoll.hpp
